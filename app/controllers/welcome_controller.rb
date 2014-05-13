1bcc99d7f24f345d7487f58639b0e0c3c863964b
citybeams <nicklohr1@gmail.com>
2014-05-13 12:37:39 -0400

    gem_file and css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ end
 
 
 
-HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-08-01&returndate=2014-08-07&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc",
+response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-08-01&returndate=2014-08-07&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc",
   :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!-3664353080892414589!1211642!0!!E2E-1"})
 
 
