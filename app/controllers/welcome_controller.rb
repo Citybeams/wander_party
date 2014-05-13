220de4cf53f4c778c5909dae7ed3a06a268fa0ad
citybeams <nicklohr1@gmail.com>
2014-05-13 13:24:48 -0400

    added date picker

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,8 +9,8 @@ end
 
 
 
-response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-08-01&returndate=2014-08-07&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc",
-  :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!-3664353080892414589!1211642!0!!E2E-1"})
+# response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=BOS&destination=LAX&departuredate=2014-08-01&returndate=2014-08-14&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc",
+#   :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!-3664353080892414589!1211642!0!!E2E-1"})
 
 
 
