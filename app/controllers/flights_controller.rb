329cb2119a3f288b544de49f4a4d914aaa9e591a
citybeams <nicklohr1@gmail.com>
2014-05-14 18:02:27 -0400

    refactor form and fix API request

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,31 @@
 class FlightsController < ApplicationController
   def index
+
   end
 
   def show
+    # flight = Flight.new
+    # flight.origin = params[]
+  end
+
+  def create
+     # capture the values from params
+      origin_input = params[:origin_input]
+    # assign values to variables
+    # put variables in a string to make an httparty request
+      destination_input = params[:destination_input]
+      depart_date = params[:depart_date]
+      return_date = params[:return_date]
+    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=#{origin_input}&destination=#{destination_input}&departuredate=#{depart_date}&returndate=#{return_date}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3664018430723002751!263206!0!!E2E-1"})
   end
 end
+
+
+
+
+https://api.test.sabre.com/v1/shop/flights?origin=LAX&destination=JFK&departuredate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&returndate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
+
+
+https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-06-01&returndate=2014-06-14&outboundflightstops=0&inboundflightstops=0&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
+
+`
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,16 @@
 class WelcomeController < ApplicationController
 
-  def self.index(origin_input, destination_input, departure_date, return_date)
+  def index
 
-    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=#{origin_input}&destination=#{destination_input}&departuredate=#{departure_date}&returndate=#{return_date}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3664018430723002751!263206!0!!E2E-1"})
-# binding.pry
-  end
-
-  def get_flight
 
   end
+
+  def create
+
+  end
+
+
+
 end
 
 # this is the url of the flight results page
