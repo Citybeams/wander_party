e143615c3006290b92b70bd0e05cbfcdc6c174b9
citybeams <nicklohr1@gmail.com>
2014-05-14 12:05:09 -0400

    more parsing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
-  def index
+  def self.index(origin_input, destination_input, departure_date, return_date)
 
-    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=BOS&destination=LAX&departuredate=2014-08-01&returndate=2014-08-14&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3664018430723002751!263206!0!!E2E-1"})
-  # binding.pry
+    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=#{origin_input}&destination=#{destination_input}&departuredate=#{departure_date}&returndate=#{return_date}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3664018430723002751!263206!0!!E2E-1"})
+# binding.pry
   end
 
   def get_flight
