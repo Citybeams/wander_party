696f066ca27d487271a862cceb1e247ddffb9d11
citybeams <nicklohr1@gmail.com>
2014-05-15 16:06:36 -0400

    finally figured out the API

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -16,16 +16,28 @@ class FlightsController < ApplicationController
       destination_input = params[:destination_input]
       depart_date = params[:depart_date]
       return_date = params[:return_date]
-    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=#{origin_input}&destination=#{destination_input}&departuredate=#{depart_date}&returndate=#{return_date}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3664018430723002751!263206!0!!E2E-1"})
+    @response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=#{origin_input}&destination=#{destination_input}&departuredate=#{depart_date}&returndate=#{return_date}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!-3663573353839303422!35306!0!!E2E-1"})
   end
 end
 
 
 
 
-https://api.test.sabre.com/v1/shop/flights?origin=LAX&destination=JFK&departuredate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&returndate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
+# .gsub(/[,]/, '-')
+
+# .map { |key, value| value }
+
+# ["{:id=>\"dateField\"}(1i)"]["{:id=>\"dateField\"}(2i)"]["{:id=>\"dateField\"}(3i)"]
+
+# .fetch("{:id=>\"dateField\"}(1i)")
 
 
-https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-06-01&returndate=2014-06-14&outboundflightstops=0&inboundflightstops=0&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
 
-`
+
+# https://api.test.sabre.com/v1/shop/flights?origin=&destination=BOS&departuredate=["2014", "5", "14"]&returndate=["2014", "5", "15"]&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=a
+
+
+# https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-06-01&returndate=2014-06-14&outboundflightstops=0&inboundflightstops=0&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
+
+
+# https://api.test.sabre.com/v1/shop/flights?origin=LAX&destination=JFK&departuredate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&returndate={"{:id=>\"dateField\"}(1i)"=>"2014", "{:id=>\"dateField\"}(2i)"=>"5", "{:id=>\"dateField\"}(3i)"=>"1"}&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc
