d3e3fc68fa60a8161c2e9a64869bdf90880da8b0
citybeams <nicklohr1@gmail.com>
2014-06-02 14:13:39 -0400

    changed API

---
+++ app/controllers/untitled.rb
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+# response["PricedItineraries"].class   returns an array
+
+# response["PricedItineraries"][0]["AirItineraryPricingInfo"]["ItinTotalFare"]["TotalFare"]["Amount"]    this returns one price
+
+# response["PricedItineraries"][0]["AirItinerary"]["OriginDestinationOptions"]["OriginDestinationOption"][0]["FlightSegment"][0]["DepartureDateTime"]
+      # this returns the departuretime
+
+
+
+response = HTTParty.get("https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2014-07-01&returndate=2014-07-14&outboundflightstops=0&inboundflightstops=0&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc", :headers => { "Authorization" => "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!-3663573353839303422!35306!0!!E2E-1
+"})
+
+
